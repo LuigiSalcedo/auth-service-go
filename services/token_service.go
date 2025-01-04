98569934f5ed4d59429968355d981d21bf36d5e7
Luigi Salcedo <csalcedoe@unicartagena.edu.co>
2025-01-04 14:53:35 -0500

    :coffin: verify token func removed

--- services/token_service.go
+++ services/token_service.go
@@ -19,23 +19,3 @@ func GenerateToken(user *models.User) string {
 
 	return token
 }
-
-func VerifyToken(tokenString string) (*models.User, error) {
-	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-		return []byte(os.Getenv("JWT_SECRET")), nil
-	})
-
-	if err != nil {
-		return nil, err
-	}
-
-	claims, ok := token.Claims.(jwt.MapClaims)
-	if !ok || !token.Valid {
-		return nil, err
-	}
-
-	user := new(models.User)
-	user.ID = claims["id"].(uint)
-
-	return user, nil
-}
