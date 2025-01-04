8ecea549596c5346191d6463eed78d1aec06e825
Luigi Salcedo <csalcedoe@unicartagena.edu.co>
2025-01-04 14:36:35 -0500

    :recycle: token payload now just has user id

--- services/token_service.go
+++ services/token_service.go
@@ -4,12 +4,13 @@ import (
 	"authentication/models"
 	"github.com/golang-jwt/jwt/v5"
 	"os"
+	"time"
 )
 
 func GenerateToken(user *models.User) string {
 	token, err := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"username": user.Username,
-		"email":    user.Email,
+		"id":  user.ID,
+		"exp": 2 * time.Hour,
 	}).SignedString([]byte(os.Getenv("JWT_SECRET")))
 
 	if err != nil {
@@ -17,4 +18,25 @@ func GenerateToken(user *models.User) string {
 	}
 
 	return token
+}
+
+func VerifyToken(tokenString string) (*models.User, error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		return []byte(os.Getenv("JWT_SECRET")), nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok || !token.Valid {
+		return nil, err
+	}
+
+	user := new(models.User)
+	user.Username = claims["username"].(string)
+	user.Email = claims["email"].(string)
+
+	return user, nil
 }
